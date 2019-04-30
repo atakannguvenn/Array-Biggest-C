612aa5c666c681fc23c99e9c742b13bb874a5e4b
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-05-01 00:59:08 +0800

    Update main.c

--- main.c
+++ main.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-        int k[10]={25,17,11,9,34,5,18,13,6,21}, temp=0,z=0,storage,storage2,max;
-        int i=0;
-        while (i<10)
-        {
-                if (k[i]>temp)
-                {
-                        temp=k[i];
-                }
-                i=i+1;
-        }
-        printf ("The highest grade is: %d",temp);
-        return 0;
+	int k[10]={25,17,11,9,34,5,18,13,6,21}, temp=0,z=0,storage,storage2,max;
+	int i=0;
+	while (i<10)
+	{
+		if (k[i]>temp)
+		{
+			temp=k[i];
+		}
+	i=i+1;
+	}
+	printf ("The highest grade is: %d",temp);
+	return 0;
 }

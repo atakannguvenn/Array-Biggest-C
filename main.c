43926b2682914a857961ccfa454844dd489e731f
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-05-01 00:47:27 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
- int main()
- {
-    int k[10]={25,17,11,9,34,5,18,13,6,21}, temp=0,z=0,storage,storage2,max;
-    int i=0;
-
+int main()
+{
+        int k[10]={25,17,11,9,34,5,18,13,6,21}, temp=0,z=0,storage,storage2,max;
+        int i=0;
         while (i<10)
         {
-            if (k[i]>temp)
-            {
-                temp=k[i];
-            }
-            i=i+1;
+                if (k[i]>temp)
+                {
+                        temp=k[i];
+                }
+                i=i+1;
         }
-    printf ("The highest grade is: %d",temp);
-    return 0;
- }
+        printf ("The highest grade is: %d",temp);
+        return 0;
+}

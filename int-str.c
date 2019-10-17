9c3d49f42058ea78c0f5c145c107b185f1b4b05c
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-10-17 13:01:36 +0800

    Update int-str.c

--- int-str.c
+++ int-str.c
@@ -1,7 +1,31 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int myAtoi(char* str)
+/*
+Programming Assignment (Due Oct 16)
+
+Write a program to simulate the addition of the following strings.
+
+char xx[10]=”12345”;
+
+char yy[10]=”678”
+
+char zz[10];
+
+Steps:
+
+1. Convert the string xx[] to it’s equivalent integer
+
+2. Convert the string yy[] to it’s equivalent integer
+
+3. Perform addition of the two integers
+
+4. Convert the sum to string zz[]
+
+5. Print the string zz[]
+*/
+
+int strToint(char* str)
 {
     int res = 0;
     for (int i = 0; str[i] != '\0'; ++i)
@@ -10,19 +34,48 @@ int myAtoi(char* str)
     return res;
 }
 
+int intTostr (int sum)
+{
+    char zz[10];
+    int i = 0, counter = -1, y;
+    int numberArray[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    y = sum;
+    while(y!=0){
+        y = y / 10;
+        counter++;
+    }
+    while (sum != 0)
+    {
+        numberArray[counter] = sum % 10;
+        sum /= 10;
+        counter--;
+    }
+    for (int y = 0; y < 5; y++){
+        zz[y] = numberArray[y] + 48;
+    }
+    printf("zz in function is %s\n", zz);
+}
+
 
 int main()
 {
     char xx[10]="12345";
     char yy[10]="678";
+    char zz[10];
     int x, y, xy;
-    x = myAtoi(xx);
-    y = myAtoi(yy);
+    x = atoi(xx);
+    y = atoi(yy);
+    //2nd method
+    //x = strToint(xx);
+    //y = strToint(yy);
     printf("x is %d, y is %d\n", x, y);
     xy = x + y;
     printf("Addition of those two variables are, %d\n", xy);
-    char zz[10];
-    sprintf(zz, "%d", xy);
+    itoa(xy, zz, 10);
+    //2nd method
+    //sprintf(zz, "%d", xy);
+    //3rd method
+    //intTostr(xy);
     printf("zz is %s\n", zz);
 
     return 0;

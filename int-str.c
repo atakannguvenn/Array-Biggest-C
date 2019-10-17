49f2d8413ef239a0ff6e7637e145cf27ed60b59e
Atakan Guven <50146105+atakannguvenn@users.noreply.github.com>
2019-10-17 18:12:44 +0800

    Update int-str.c

--- int-str.c
+++ int-str.c
@@ -3,25 +3,15 @@
 
 /*
 Programming Assignment (Due Oct 16)
-
 Write a program to simulate the addition of the following strings.
-
 char xx[10]=”12345”;
-
 char yy[10]=”678”
-
 char zz[10];
-
 Steps:
-
 1. Convert the string xx[] to it’s equivalent integer
-
 2. Convert the string yy[] to it’s equivalent integer
-
 3. Perform addition of the two integers
-
 4. Convert the sum to string zz[]
-
 5. Print the string zz[]
 */
 
@@ -44,15 +34,16 @@ int intTostr (int sum)
         y = y / 10;
         counter++;
     }
+    int counter2 = counter + 1;
     while (sum != 0)
     {
         numberArray[counter] = sum % 10;
         sum /= 10;
         counter--;
     }
-    for (int y = 0; y < 5; y++){
+    zz[counter2] = '\0';
+    for (int y = 0; y < counter2; y++)
         zz[y] = numberArray[y] + 48;
-    }
     printf("zz in function is %s\n", zz);
 }
 
@@ -63,20 +54,12 @@ int main()
     char yy[10]="678";
     char zz[10];
     int x, y, xy;
-    x = atoi(xx);
-    y = atoi(yy);
-    //2nd method
-    //x = strToint(xx);
-    //y = strToint(yy);
+    x = strToint(xx);
+    y = strToint(yy);
     printf("x is %d, y is %d\n", x, y);
     xy = x + y;
     printf("Addition of those two variables are, %d\n", xy);
-    itoa(xy, zz, 10);
-    //2nd method
-    //sprintf(zz, "%d", xy);
-    //3rd method
-    //intTostr(xy);
-    printf("zz is %s\n", zz);
+    intTostr(xy);
 
     return 0;
 
